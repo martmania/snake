05c7e4fbc281d555b81d2ff4fd64f01621489a8c
doronova_mv <doronova_mv@transset.ru>
2016-12-12 15:32:38 +0300

    Инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,26 +10,57 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x=" + x);
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x=" + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x=" + x);
+
             Point p1 = new Point(1,3,'*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
-            p1.Draw();
-            
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1,y =" + p1.y);
 
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '@';
-            p2.Draw();
-           
+           Point p2 = new Point(4, 5, '@');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1=p2. p1.x = " + p1.x + ", p1,y =" + p1.y+"; p2.x = " + p2.x + ", p2.y = " + p2.y);
 
-         
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1,y =" + p1.y);
 
             Console.ReadLine();
         }
-      
+        public static void Func1(int value)
+        {
+
+        }
+
+        public static void Func2(int value)
+        {
+
+        }
+        public static void Func3(int value)
+        {
+
+        }
+        public static void Move(int value)
+        {
+
+        }
+        public static void Update(char value)
+        {
+
+        }
     }
 
 }
