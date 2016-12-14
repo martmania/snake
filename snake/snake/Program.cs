fbc8dff3c5423b5cb9bea8d27993bec8db152e51
doronova_mv <doronova_mv@transset.ru>
2016-12-14 15:49:39 +0300

    Горизонтальные линии

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -21,24 +21,8 @@ namespace snake
             p2.Draw();
 
 
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-
-            foreach(int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-
-            numlist.RemoveAt(0);
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
+            HorizontaiLine line = new HorizontaiLine(5,10,8,'+');
+            line.Drow();
 
             Console.ReadLine();
         }
