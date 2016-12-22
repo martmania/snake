0031d5afd10dd3f02e055792ac3ab543519c065f
doronova_mv <doronova_mv@transset.ru>
2016-12-22 10:47:00 +0300

    1

---
+++ snake/snake/Walls.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Walls
+    {
+        List<Figures> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figures>();
+            HorizontaiLine upLine = new HorizontaiLine(0, mapWidth - 2, 0, '+');
+            HorizontaiLine downLine = new HorizontaiLine(0, mapWidth - 2, mapHeight - 1, '+');
+            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+
+            wallList.Add(upLine);
+            wallList.Add(downLine);
+            wallList.Add(leftLine);
+            wallList.Add(rightLine);
+        }
+
+        internal bool IsHit(Figures figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
